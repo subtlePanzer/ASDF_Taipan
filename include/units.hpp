237f3677c1799ac1b97269dedf0cc543465f7f6a
SubtlePanzer <rdl132345@gmail.com>
2026-08-31 18:05:20 +0930

    added units class

---
+++ include/units.hpp
@@ -0,0 +1,57 @@
+// -----------------------------------------------------------------------------
+// ASDF 2026-2027 'Taipan' language - (c) 2026 Riley Lorenz  & ASDF Robotics
+// -----------------------------------------------------------------------------
+
+#ifndef asdf_units_h
+#define asdf_units_h
+
+#include <ratio>
+
+template <typename internal_type, typename conversion_factor = std::ratio<1>>
+class length_value {
+public:
+        length_value(void) {
+                internal = 0;
+                this_ratio = conversion_factor::num / conversion_factor::den;
+        }
+
+        length_value(internal_type v) : internal(v) {
+                this_ratio = conversion_factor::num / conversion_factor::den;
+        }
+
+        template <typename other_ratio>
+        length_value(const length<internal_type, ratio>& l) {
+                double other_ratio = (double)other_ratio::num / (double)other_ratio::den;
+
+                internal = l.internal() * other_ratio / this_ratio;
+        };
+
+        operator internal_type() const {
+                return (internal_type)internal;
+        }
+
+        internal_type internal() const {
+                return internal;
+        }
+
+        length_value operator+(const length_value& l) const {
+                return length_value(internal + l.internal);
+        }
+
+        length_value operator-(const length_value& l) const {
+                return length_value(internal - l.internal);
+        }
+
+private:
+        internal_type internal;
+        double this_ratio;
+};
+
+using meters = length_value<double, std::ratio<1000, 1>>;
+using mm = length_value<double, std::ratio<1>>; // using millis as base unit
+using in = length_value<double, std::ratio<10, 254>>;
+using ft = length_value<double, std::ratio<10, 254 * 12>>;
+
+// add custom literals
+
+#endif
