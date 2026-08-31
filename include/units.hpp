0eb0cd56c1201ca116a91f1ce0f697a7a083a182
SubtlePanzer <rdl132345@gmail.com>
2026-08-31 19:43:58 +0930

    fixed unintentional variable shadowing

--- include/units.hpp
+++ include/units.hpp
@@ -11,39 +11,39 @@ template <typename type, typename internal_type = double, typename conversion_fa
 class value {
 public:
         constexpr value(void) {
-                internal = 0;
+                _internal = 0;
                 this_ratio = conversion_factor::num / conversion_factor::den;
         }
 
-        constexpr value(internal_type v) : internal(v) {
+        constexpr value(internal_type v) : _internal(v) {
                 this_ratio = conversion_factor::num / conversion_factor::den;
         }
 
         template <typename other_ratio>
-        value(const length<internal_type, other_ratio>& l) {
-                double other_ratio = (double)other_ratio::num / (double)other_ratio::den;
+        value(const value<type, internal_type, other_ratio>& l) {
+                double ratio_double = (double)other_ratio::num / (double)other_ratio::den;
 
-                internal = l.internal() * other_ratio / this_ratio;
+                _internal = l._internal() * ratio_double / this_ratio;
         };
 
         constexpr operator internal_type() const {
-                return (internal_type)internal;
+                return (internal_type)_internal;
         }
 
         constexpr internal_type internal() const {
-                return internal;
+                return _internal;
         }
 
-        constexpr value operator+(const length_value& l) const {
-                return value(internal + l.internal);
+        constexpr value operator+(const value& l) const {
+                return value(_internal + l._internal);
         }
 
         constexpr value operator-(const value& l) const {
-                return value(internal - l.internal);
+                return value(_internal - l._internal);
         }
 
 private:
-        internal_type internal;
+        internal_type _internal;
         double this_ratio;
 };
 
@@ -55,7 +55,7 @@ template<typename ratio = std::ratio<1>>
 using length = value<_length, double, ratio>;
 
 template<typename ratio = std::ratio<1>>
-using time = value<_time, double, ratio>;
+using asdf_time = value<_time, double, ratio>;
 
 template<typename ratio = std::ratio<1>>
 using angle = value<_angle, double, ratio>;
@@ -65,8 +65,8 @@ using meters = length<std::ratio<1000, 1>>;
 using in = length<std::ratio<10, 254>>;
 using ft = length<std::ratio<10, 254 * 12>>;
 
-using sec = time<std::ratio<1000, 1>>;
-using msec = time<std::ratio<1>>; // msec is base, as above
+using asdf_sec = asdf_time<std::ratio<1000, 1>>;
+using asdf_msec = asdf_time<std::ratio<1>>; // msec is base, as above
 
 using rad = angle<std::ratio<1>>; // #radian_primacy
 using deg = angle<std::ratio<57296, 1000>>;
@@ -90,12 +90,12 @@ ft operator"" _ft(long double val) {
         return ft((double)val);
 }
 
-sec operator"" _sec(long double val) {
-        return sec((double)val);
+asdf_sec operator"" _sec(long double val) {
+        return asdf_sec((double)val);
 }
 
-msec operator"" _msec(long double val) {
-        return msec((double)val);
+asdf_msec operator"" _msec(long double val) {
+        return asdf_msec((double)val);
 }
 
 rad operator"" _rads(long double val) {
